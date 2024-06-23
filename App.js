c75d0c7f03092b47054d2bfb56ceea3d0146258a
integgy <matthewregidor@gmail.com>
2024-06-23 14:49:39 -0700

    add app logos

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button, Alert} from 'react-native';
+
 
 export default function App() {
+  const logo = require("./assets/app_images/katie-pack-logo-cropped.png")
+  const handle_press = () => {
+    console.log("hello world")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image source={logo}/>
+      {/* <Text onPress={handle_press}>Hello World</Text> */}
+      <Button
+        color="green"
+        title='Get Started'
+        onPress={()=> Alert.alert("Welcome to Katie Pack!")}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#faebd7',
     alignItems: 'center',
     justifyContent: 'center',
   },
