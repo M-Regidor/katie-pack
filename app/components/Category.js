1a3b2457b0fbc8488a5e285b7bf5ef5b2c4d5d8f
integgy <matthewregidor@gmail.com>
2024-06-30 17:10:34 -0700

    Add packing details

--- app/components/Category.js
+++ app/components/Category.js
@@ -1,33 +1,80 @@
-import { Image, TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { Image, TouchableOpacity, Text, StyleSheet, View } from 'react-native'
 import React from 'react'
 
-export default function Category({imagePath, navigation, title}) {
-  return (
-    <TouchableOpacity 
-        style={styles.categoryContainer}
-        onPress={()=> navigation.navigate(title)}
-    >
-        <Image source={imagePath} style={styles.iconImage}/>
-        <Text style={styles.listItemText}>{title}</Text>
-    </TouchableOpacity>
-  )
+export default function Category({imagePath, navigation, title, itemList}) {
+    const total = itemList.length
+    let packed = 0
+    let missing = 0
+
+    itemList.forEach(item => {
+        item.packed ? packed ++ : missing ++    
+    });
+
+    return (
+        <TouchableOpacity onPress={()=> navigation.navigate(title)} style={styles.categoryContainer}>
+            <View style={styles.iconContainer}>
+                <Image source={imagePath} style={styles.iconImage}/>
+                <Text style={styles.listItemText}>{title}</Text>
+            </View>
+            <View style={styles.countContainer}>
+                <View style={styles.countDetails}>
+                    <Text style={styles.countNum}>{packed}</Text>
+                    <Text style={styles.countText}>Packed</Text>
+                </View>
+                <View style={styles.countDetails}>
+                    <Text style={styles.countNum}>{missing}</Text>
+                    <Text style={styles.countText}>Missing</Text>
+                </View>
+                <View style={styles.countDetails}>
+                    <Text style={styles.countNum}>{total}</Text>
+                    <Text style={styles.countText}>Total</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
     categoryContainer:{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: 167,
+        borderRadius: 10,
+        backgroundColor: "#d8bfd8"
+    },
+    iconContainer:{
         height: 150,
         width: 150,
-        // borderWidth: 1,
-        // borderRadius: 10,
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center"
     },
     listItemText:{
-        fontSize: 20
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white"
     },
     iconImage: {
         width: "100%",
         height: "90%"
+    },
+    countContainer: {
+        width: 265,
+        flexDirection: "row",
+        justifyContent: "space-evenly"
+    },
+    countDetails: {
+        alignItems: "center",
+    },
+    countText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white"
+    },
+    countNum: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white"
     }
 })
--- app/screens/PackingList.js
+++ app/screens/PackingList.js
@@ -3,12 +3,14 @@ import React from 'react'
 import { storeData } from '../store/AsyncStorage'
 import { useAppStore } from '../store/useAppStore'
 import Category from '../components/Category'
+import useToiletriesStore from '../store/useToiletriesStore'
 
 
 export default function PackingList({navigation}) {
   const toiletries = require("../assets/item_icons/Toiletries.png")
   const username = useAppStore(state => state.username)
   const updateUsername = useAppStore(state => state.updateUsername)
+  const toiletriesArray = useToiletriesStore(state => state.getToiletriesArray(state))
 
   const handleEnter = () => {
     updateUsername(null)
@@ -18,10 +20,15 @@ export default function PackingList({navigation}) {
 
   return (
       <SafeAreaView style={styles.screen}>
-        <Text style={styles.text}>Packing List</Text>
+        <Text style={styles.text}>{username}'s Packing List</Text>
         <Button title='Remove username' onPress={handleEnter}/>
         <View style={styles.listContainer}>
-          <Category imagePath={toiletries} title={"Toiletries"} navigation={navigation}/>
+          <Category 
+            imagePath={toiletries}
+            title={"Toiletries"}
+            navigation={navigation}
+            itemList={toiletriesArray}
+          />
         </View>
       </SafeAreaView>
     )
@@ -37,7 +44,7 @@ const styles = StyleSheet.create({
   },
   listContainer:{
     flex: 1,
-    borderWidth: 1,
+    // borderWidth: 1,
     paddingTop: 10
   },
   listItemText:{
